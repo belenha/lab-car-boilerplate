77ece58b4cfc17adf4a990b3c654b832df86c51a
belenha <lubinatica@gmail.com>
2018-06-18 17:33:26 -0500

    ultimos cambios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  // var instance = M.Modal.getInstance(elem);
+
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.modal');
+  //   var instances = M.Modal.init(elems, options);
+  // });
